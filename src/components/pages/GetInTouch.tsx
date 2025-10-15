72682979888f6dcc0ef8fa21a980411cb15d6ad5
ajasma6570 <ajasmaprince@gmail.com>
2025-10-15 16:46:55 +0530

    conatct page font updated

--- src/components/pages/GetInTouch.tsx
+++ src/components/pages/GetInTouch.tsx
@@ -70,60 +70,60 @@ export default function GetInTouch() {
   };
 
   return (
-    <>
+    <div className="pt-20">
       <ToastContainer />
 
       <section
         ref={sectionRef}
         className="bg-background text-center py-24 px-6"
       >
-        <p className="contact-animate font-deltha text-sm tracking-widest text-neutral-400 mt-14 mb-4">
+        <p className="text-sm md:text-xl pt-10 tracking-[0.3rem] md:tracking-[0.5rem] mb-3 uppercase font-paragraph font-[300] text-gold">
           CONTACT US
         </p>
 
-        <h1 className="contact-animate font-deltha text-6xl md:text-8xl font-extrabold text-white mb-6 leading-none">
+        <h1 className="contact-animate tracking-[0.2rem] md:tracking-[0.8rem] leading-[1.3] uppercase font-heading font-bold text-6xl md:text-8xl text-white mb-6">
           GET IN TOUCH
         </h1>
 
-        <p className="contact-animate exclusive-text text-base md:text-lg text-silver max-w-2xl mx-auto mb-12">
+        <p className="contact-animate font-paragraph tracking-[0.2rem] font-[300] text-base md:text-lg text-silver max-w-2xl mx-auto mb-12">
           Reach out to start the conversation, schedule a consultation, or ask
           any questions.
         </p>
 
         {/* Social Icons */}
         <div className="flex justify-center gap-6">
-          <a
+          <Link
             href="https://instagram.com"
             target="_blank"
             rel="noopener noreferrer"
             className="w-12 h-12 flex items-center justify-center rounded-full  text-silver hover:text-gold transition-all duration-300"
           >
             <FaInstagram className="h-10 w-10" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://x.com"
             target="_blank"
             rel="noopener noreferrer"
             className="w-12 h-12 flex items-center justify-center rounded-full  text-silver hover:text-gold transition-all duration-300"
           >
             <FaXTwitter className="h-10 w-10" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://linkedin.com"
             target="_blank"
             rel="noopener noreferrer"
             className="w-12 h-12 flex items-center justify-center rounded-full text-silver hover:text-gold transition-all duration-300"
           >
             <FaLinkedinIn className="h-10 w-10" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://facebook.com"
             target="_blank"
             rel="noopener noreferrer"
             className="w-12 h-12 flex items-center justify-center rounded-full text-silver hover:text-gold transition-all duration-300"
           >
             <FaFacebookF className="h-10 w-10" />
-          </a>
+          </Link>
         </div>
       </section>
 
@@ -144,7 +144,7 @@ export default function GetInTouch() {
         <div className="relative z-10 w-full max-w-2xl bg-white p-5 sm:p-10 exclusive-text rounded-xl sm:rounded-2xl ">
           <form className="space-y-8" onSubmit={handleSubmit}>
             <div className="border-b border-background pb-2">
-              <label className="block text-sm font-medium text-background mb-1">
+              <label className="block text-sm text-background mb-1 font-paragraph font-[300]">
                 NAME
               </label>
               <input
@@ -156,7 +156,7 @@ export default function GetInTouch() {
             </div>
 
             <div className="border-b border-background pb-2">
-              <label className="block text-sm font-medium text-background mb-1">
+              <label className="block text-sm text-background mb-1 font-paragraph font-[300]">
                 EMAIL ADDRESS
               </label>
               <input
@@ -168,7 +168,7 @@ export default function GetInTouch() {
             </div>
 
             <div className="border-b border-background pb-2">
-              <label className="block text-sm font-medium text-background mb-1">
+              <label className="block text-sm font-paragraph font-[300] text-background mb-1">
                 SUBJECT
               </label>
               <input
@@ -180,7 +180,7 @@ export default function GetInTouch() {
             </div>
 
             <div className="border-b border-background pb-2">
-              <label className="block text-sm font-medium text-background mb-1">
+              <label className="block text-sm font-paragraph font-[300] text-background mb-1">
                 MESSAGE
               </label>
               <textarea
@@ -194,7 +194,7 @@ export default function GetInTouch() {
             <div className="pt-4">
               <button
                 type="submit"
-                className="group w-full justify-center cursor-pointer inline-flex bg-gold hover:bg-gold-dark text-white px-8 py-3 text-lg font-medium transition-colors duration-300 [transform:skewX(-20deg)]"
+                className="group w-full justify-center cursor-pointer inline-flex bg-gold hover:bg-gold-dark text-white px-8 py-3 text-lg font-heading tracking-[0.2rem] transition-colors duration-300 [transform:skewX(-20deg)]"
               >
                 <span className="flex items-center gap-2 [transform:skewX(20deg)]">
                   SEND MESSAGE
@@ -207,48 +207,54 @@ export default function GetInTouch() {
 
       <section className="bg-background text-center py-10 sm:py-24 px-6">
         {/* Small Heading */}
-        <p className="text-sm font-deltha tracking-widest text-silver mt-14 mb-4">
+        <p className="text-sm font-paragraph tracking-[0.3rem] font-[400] text-silver mt-14 mb-4">
           OUR LOCATIONS
         </p>
 
         {/* Main Title */}
-        <h1 className="text-6xl font-deltha md:text-8xl font-extrabold text-gold mb-8 leading-none">
+        <h1 className=" text-4xl lg:text-6xl font-heading tracking-[0.25rem] md:text-8xl text-gold mb-8 leading-[1.5]">
           VISIT US
         </h1>
 
         {/* Subtext */}
-        <p className="text-base md:text-lg exclusive-text text-silver max-w-2xl mx-auto mb-12">
+        <p className="text-base md:text-lg font-paragraph tracking-[0.2rem] leading-[1.5] text-silver max-w-2xl mx-auto mb-12">
           Visit us at one of our conveniently located studios, where our team is
           ready to discuss your design goals and explore possibilities for your
           space.
         </p>
       </section>
 
-      <section className="bg-background py-10 sm:py-20">
+      <section className="bg-background pb-10 sm:py-20">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 px-6 lg:px-12 items-center">
           {/* Left Side: Office Details (1/3) */}
           <div className="flex flex-col justify-center relative md:col-span-1">
             <div className="pl-6 space-y-12">
               {/* Office Block */}
               <div>
-                <h2 className=" font-xl font-deltha font-bold text-gold uppercase tracking-wide">
+                <h2 className=" font-xl font-deltha font-bold text-gold uppercase tracking-[0.2rem]">
                   Head Office
                 </h2>
-                <p className="text-white exclusive-text mt-2 leading-relaxed text-base sm:text-lg">
-                  Al Eliza Interior, <br />
-                  FBL Business Center, <br />
-                  Al Mamzar - Dubai - UAE
+                <p className="text-white mt-2 text-base sm:text-lg cursor-pointer font-paragraph tracking-[0.2rem] font-[300] hover:text-gold transition-colors duration-300">
+                  <Link
+                    href="https://maps.app.goo.gl/5GJHZDmpqdpkJmtb7"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Al Eliza Interior, <br />
+                    FBL Business Center, <br />
+                    Al Mamzar - Dubai - UAE{" "}
+                  </Link>
                 </p>
               </div>
 
               {/* Email */}
               <div className="exclusive-text">
-                <h2 className="text-xl font-deltha font-bold text-gold uppercase tracking-wide">
+                <h2 className="text-xl font-deltha font-bold text-gold uppercase tracking-[0.2rem]">
                   Email
                 </h2>
                 <Link
                   href="mailto:info@alelizainteriors.com"
-                  className="block text-white mt-2 text-base sm:text-xl"
+                  className="block text-white mt-2 text-base sm:text-xl font-paragraph tracking-[0.2rem] font-[300] hover:text-gold transition-colors duration-300"
                 >
                   info@alelizainteriors.com
                 </Link>
@@ -256,20 +262,20 @@ export default function GetInTouch() {
 
               {/* Phone */}
               <div className="exclusive-text">
-                <h2 className="text-xl font-deltha font-bold text-gold uppercase tracking-wide">
+                <h2 className="text-xl font-deltha font-bold text-gold uppercase tracking-[0.2rem]">
                   Call Us
                 </h2>
 
                 <div className="mt-2 space-y-1">
                   <Link
                     href="tel:+971522889300"
-                    className="block text-white text-base sm:text-xl"
+                    className="block text-white text-base sm:text-xl font-paragraph tracking-[0.2rem] font-[300] hover:text-gold transition-colors duration-300"
                   >
                     +971 522 889 300
                   </Link>
                   <Link
                     href="tel:+971543783000"
-                    className="block  text-white text-base sm:text-xl"
+                    className="block  text-white text-base sm:text-xl font-paragraph tracking-[0.2rem] font-[300] hover:text-gold transition-colors duration-300"
                   >
                     +971 54 378 3000
                   </Link>
@@ -279,17 +285,16 @@ export default function GetInTouch() {
           </div>
 
           {/* Right Side: Office Image (2/3) */}
-          <div className="relative w-full h-[350px] md:h-[450px] lg:h-[500px] md:col-span-2 ">
-            <Image
-              src="https://res.cloudinary.com/dxhmpdgqj/image/upload/v1753375127/about_2_ucqtyb.webp"
-              alt="Head Office"
-              fill
-              className="object-cover rounded-xl"
-              priority
-            />
+          <div className="relative w-full max-w-2xl ml-auto h-[350px] md:h-[450px] lg:h-[500px] md:col-span-2 rounded-xl overflow-hidden">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d225.46651462006213!2d55.35785207844792!3d25.28860235409731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d931d237c6d%3A0xc429f45c56e9c88!2sAl%20Eliza%20interior!5e0!3m2!1sen!2sae!4v1753894309606!5m2!1sen!2sae"
+              loading="lazy"
+              title="Al Eliza Interior Location"
+              className="text-center w-full h-full"
+            ></iframe>
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 }
--- src/components/pages/Portfolio.tsx
+++ src/components/pages/Portfolio.tsx
@@ -30,12 +30,12 @@ export default function Portfolio() {
   };
 
   return (
-    <div className="bg-background text-white mb-8">
+    <div className="bg-background text-white mb-8 pt-20">
       <section className="flex flex-col items-center justify-start pt-20 md:pt-40 px-4 md:px-6">
-        <h1 className="text-sm md:text-xl tracking-[0.3rem] md:tracking-[0.6rem] uppercase font-deltha font-light">
+        <h1 className="text-sm md:text-xl tracking-[0.3rem] md:tracking-[0.5rem] uppercase font-paragraph font-[300] text-gold">
           Signature Projects
         </h1>
-        <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-4 max-w-5xl text-center tracking-[0.2rem] md:tracking-[0.8rem] leading-snug uppercase font-deltha font-bold">
+        <p className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-4 max-w-5xl text-center tracking-[0.2rem] md:tracking-[0.8rem] leading-[1.5] uppercase font-heading font-bold">
           Designed with Intention
         </p>
       </section>
@@ -50,7 +50,7 @@ export default function Portfolio() {
                 selectedTab === tab.value
                   ? "border-gold text-gold"
                   : "border-transparent text-white hover:text-gold"
-              } font-deltha uppercase tracking-wider md:tracking-widest transition-colors duration-300 cursor-pointer`}
+              } font-paragraph  uppercase tracking-[0.2rem] font-[300] transition-colors duration-300 cursor-pointer`}
               onClick={() => setSelectedTab(tab.value)}
             >
               {tab.label}
@@ -68,40 +68,35 @@ export default function Portfolio() {
               visible: { transition: { staggerChildren: 0.1 } },
             }}
           >
-            {filteredProjects.map((project) => {
-              const imagesToShow =
-                selectedTab === "all" ? [project.images[0]] : project.images;
-
-              return imagesToShow.map((img, index) => (
-                <motion.div
-                  key={`${project.id}-${index}`}
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, scale: 0.9 }}
-                  transition={{
-                    duration: 0.4,
-                    delay: index * 0.05,
-                  }}
-                  layout
-                  className="group relative overflow-hidden cursor-pointer"
-                  onClick={() => handleImageClick(project, index)}
-                >
-                  <div className="relative w-full h-56 sm:h-64 overflow-hidden">
-                    <Image
-                      src={img}
-                      alt={project.title}
-                      fill
-                      className="object-cover object-center transition-transform duration-500 ease-in-out group-hover:scale-110"
-                    />
-                    <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
-                    <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent"></div>
-                    <h3 className="absolute bottom-3 md:bottom-7 left-3 md:left-4 text-white text-sm md:text-base tracking-[0.1rem] md:tracking-[0.2rem] exclusive-text z-10">
-                      {project.title}
-                    </h3>
-                  </div>
-                </motion.div>
-              ));
-            })}
+            {filteredProjects.map((project, index) => (
+              <motion.div
+                key={`${project.id}-${index}`}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, scale: 0.9 }}
+                transition={{
+                  duration: 0.4,
+                  delay: index * 0.05,
+                }}
+                layout
+                className="group relative overflow-hidden cursor-pointer"
+                onClick={() => handleImageClick(project, index)}
+              >
+                <div className="relative w-full h-56 sm:h-64 overflow-hidden">
+                  <Image
+                    src={project.images[0]}
+                    alt={project.title}
+                    fill
+                    className="object-cover object-center transition-transform duration-500 ease-in-out group-hover:scale-110"
+                  />
+                  <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
+                  <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent"></div>
+                  <h3 className="absolute bottom-3 md:bottom-7 left-3 md:left-4 text-white text-lg md:text-lg tracking-[0.1rem] md:tracking-[0.2rem] font-paragraph font-[300] z-10">
+                    {project.title}
+                  </h3>
+                </div>
+              </motion.div>
+            ))}
           </motion.div>
         </AnimatePresence>
 
