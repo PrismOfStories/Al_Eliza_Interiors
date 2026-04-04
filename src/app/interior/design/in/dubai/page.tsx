eb63c3d647ab36800b83974f535f9c86e298f8a7
PrismOfStories <prismofstories2025@gmail.com>
2026-04-05 03:15:22 +0530

    add test comment

--- src/app/interior/design/in/dubai/page.tsx
+++ src/app/interior/design/in/dubai/page.tsx
@@ -66,6 +66,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Page() {
+  // page data list
   const features = [
     "Custom designs tailored to your lifestyle",
     "Realistic 3D room design before execution",
